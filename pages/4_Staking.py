e4e18ccd2fff31161b8294ba865731031c6996a5
Alireza Samari <110922616+AlirezaSamari@users.noreply.github.com>
2023-01-04 13:23:17 +0330

    Update 4_Staking.py

--- pages/4_Staking.py
+++ pages/4_Staking.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 st.set_page_config( 
-    page_title="NFT", 
+    page_title="Stake", 
     page_icon=":bar_chart:", 
     layout="wide"
 )
